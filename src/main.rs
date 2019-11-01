7745839cba7a9bf4ea1080f6641cac71837b5d1d
vincent <perrier.vincent0210@gmail.com>
2019-11-01 07:35:53 -0400

    Made the preference feature. On branch master 	modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/main.rs 	modified:   src/preferences.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
 mod preferences;
+#[macro_use]
+extern crate serde_json;
 
 use std::net::SocketAddr;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
-    preferences::create_create_default_file();
-    println!("{}", preferences::get_server_info().unwrap());
+
+    /* Get command line arguments */
+    let args: Vec<String> = env::args().collect();
+    
+
+    println!("[Main]: Vincent Perrier Rust Server Backend.");
+    let pref = preferences::get_server_info().unwrap();
+    start(pref.0, pref.1);
+
+}
+
+fn start(address: SocketAddr, max_thread_count: u16)
+{
+    println!("[Main]: Got {} to bind the server.", max_thread_count);
+    let num_threads_max: u16 = max_thread_count;
+    let ret = std::net::TcpListener::bind(address);
+    let listener = match ret
+    {
+        Ok(listener) => {listener},
+        Err(err) => {println!("[Main]: could not bind address. Check admin privileges."); panic!(err)}
+    };
+    for tcp_streams in listener.incoming()
+    {
+        handle_client(tcp_streams.unwrap());
+    }
+}
+
+fn handle_client(stream: std::net::TcpStream)
+{
+    println!("[Main]: Incoming connections from: {}", stream.peer_addr().unwrap());
 }
--- src/preferences.rs
+++ src/preferences.rs
@@ -1,5 +1,7 @@
 #[allow(unused_imports)]
 use serde_json::{Result, Value};
+use serde::Serialize;
+use serde::Deserialize;
 
 #[allow(unused_imports)]
 use std::net::{SocketAddr, ToSocketAddrs};
@@ -19,64 +21,76 @@ use std::io::prelude::*;
 use std::vec::Vec;
 #[allow(unused_imports)]
 use std::vec::Splice;
+use std::path::Path;
 
+
+/* Serde json needs this macro to know the type of the json
+object we are saving and reading.*/
+
+/* Easy  way to remove the "" in the returned string.
+Because now it is explicitly a string */
+#[derive(Serialize, Deserialize)]
+struct Pref {
+    ip: String,
+    port: u16,
+    num_threads_max: u16,
+}
 
 /* Returns a tcp SocketAddr type describing the user config */
-pub fn get_server_info() -> Result<String>
+pub fn get_server_info() ->std::result::Result<(SocketAddr, u16), &'static str>
 {
     /* Open file*/
-print!("1\n");
-
-    let ret = File::open("./pref.json");
-    let preferences_file =
-    match ret {
-        Ok(file_handle) => file_handle,
-        Err(_err) => {  println!("{}", _err);
-                        create_create_default_file();
-                        File::open("./pref.json").unwrap()},
+    let ret = File::open(Path::new("./pref.json"));
+    /* If file doesn't exist */
+    let preferences_file = match ret{
+        Ok(handle) => handle,
+        Err(err) => {println!("File error: {}",err);create_default_file(); File::open(Path::new("./pref.json")).unwrap()}
     };
-print!("1\n");
-        /* Get port and socket information from a JSON file*/
-        let mut reader = std::io::BufReader::new(preferences_file);
-print!("1\n");
 
-        let mut content_string: String = String::new();
-print!("1\n");
-        reader.read_to_string(&mut content_string).unwrap();
-print!("1\n");
-        let json_object: Value = serde_json::from_str(&content_string)?;
-print!("1\n");
+
+    /* Get port and socket information from a JSON file*/
+    let mut reader = std::io::BufReader::new(preferences_file);
+
+    let mut content_string: String = String::new();
+
+    reader.read_to_string(&mut content_string).unwrap();
+
+    let json_object = serde_json::from_str(&content_string);
+
+    if json_object.is_ok(){
+        /* Reading a json object of type pref */
+        let objects: Pref = json_object.unwrap();
         /* Parse JSON */
-        let ip_string = format!("{}:{}", json_object["ip"].as_str().unwrap().to_string(),
-        json_object["port"].as_str().unwrap().to_string());
-print!("1\n");
-        Ok(ip_string)
+        let ip_string = format!("{}:{}", objects.ip, objects.port);
+        println!("[Pref]: Read {} from pref.json", ip_string);
+        let ret: SocketAddr = ip_string.parse().unwrap();
+        Ok((ret, objects.num_threads_max))
+    }
+    else {
+        Err("Couldn't get preferences. Check Json formatting")
+    }
+
 }
 
 /* Will panic if file cannot be created in the same directory */
-pub fn create_create_default_file()
+pub fn create_default_file()
 {
     /* create a dummy JSON preference file */
-    let json_file = r#"{
-        "ip": "127.0.0.1",
-        "port": 80
-    }"#;
-    print!("2\n");
+    let json_file = json!( {"ip":"127.0.0.1","port":80, "num_threads_max":20} );
     /* Create the file */
     let file = match File::create("./pref.json")
     {
         Ok(file_handle) => file_handle,
         Err(_err) => panic!("Check files permissions, could not write preferences file.")
     };
-    print!("2\n");
     /* Convert the serde strructure to a rust string  */
-    let data = serde_json::to_string(&json_file).unwrap();
-    print!("2\n");
+    let data = serde_json::to_string_pretty(&json_file).unwrap();
     /* prepare the writing buffer */
     let mut writer = std::io::BufWriter::new(file);
-    print!("2\n");
     /* write the rust string with json in it to file system*/
     writer.write(data.as_bytes()).unwrap();
-    print!("2\n");
     /* writer & file going out of scope, the file is going to be closed and dropped */
+    writer.flush().unwrap();
+    /*  Make sure to drop the buffer writer */
+    std::mem::drop(writer);
 }
